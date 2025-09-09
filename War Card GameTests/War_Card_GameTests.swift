dcb23171a4d0c3880f38962a11442097f0760d9d
idrissyiroj <202151175@std.umk.ac.id>
2025-09-09 16:37:15 +0700

    Initial Commit

---
+++ War Card GameTests/War_Card_GameTests.swift
@@ -0,0 +1,16 @@
+//
+//  War_Card_GameTests.swift
+//  War Card GameTests
+//
+//  Created by Idris Syiroj on 08/09/25.
+//
+
+import Testing
+
+struct War_Card_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
